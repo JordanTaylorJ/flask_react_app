c7a65486a9d36c8cf547c18e00ab075f89d9c563
JordanJoseph <jordant.joseph@outlook.com>
2023-11-28 14:39:38 -0700

    update migrate in app.py to render_as_batch in order to properly update migration files after changes are made, flask db init to remake migration files, add a Log model to models.py with a foreign key for user one-to-many relationship, migrate and reseed db

--- server/models.py
+++ server/models.py
@@ -1,7 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy 
 from sqlalchemy import MetaData 
 
-metadata = MetaData()
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
 
 db = SQLAlchemy(metadata=metadata)
 
@@ -11,7 +13,18 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(3), unique=True)
     password = db.Column(db.String(3))
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    logs = db.relationship('Log', backref='log')
 
     def __repr__(self):
         return f'<User {self.name}>'
+    
+class Log(db.Model):
+    __tablename__ = 'logs'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id')) 
+
     

456785df12b34a5d12d9fabe9c3b85d9acb518d6
JordanJoseph <jordant.joseph@outlook.com>
2023-12-01 23:07:13 -0700

    create profile component and directory, update link on Menu

--- client/src/app/components/Menu.jsx
+++ client/src/app/components/Menu.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 const Menu = (setIsOpen) => {
     return(
         <div className=' p-4 text-lg container fixed z-40'>
-            <Link className='block hover:text-blue-200'href='/' onClick={(() => setIsOpen(false))}>Profile</Link>
+            <Link className='block hover:text-blue-200'href='/profile' onClick={(() => setIsOpen(false))}>Profile</Link>
             <Link className='block hover:text-blue-200' href='/logs' onClick={(() => setIsOpen(false))}>Logs</Link>
             <Link className='block hover:text-blue-200' href='/' onClick={(() => setIsOpen(false))}>Aircraft</Link>
             <button className='block hover:text-blue-200'>Logout</button>
---
+++ client/src/app/profile/page.js
@@ -0,0 +1,10 @@
+const Profile = () => {
+    return(
+        <main className='p-24'>
+            <h2>User profile here</h2>
+            <p>settings?, update user info, summary of hours, medical certificate info?</p>
+        </main>
+    )
+}
+
+export default Profile;
